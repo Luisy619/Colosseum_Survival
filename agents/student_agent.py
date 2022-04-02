e73a319a06b18296655937c92708e76726f8aa4a
Max <maxboucher45@gmail.com>
2022-04-02 15:06:31 -0400

    Implemented "endCheck"
    - A function for the BoardState class. Will detect whether or not the state in a simulation is the end or not, and returns an appropriate value for use in the MCTS tree.

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -16,6 +16,8 @@ class BoardState:
             self.myPosition = my_pos;
             self.advPosition = adv_pos;
             self.maxStep = max_step;
+            self.board_size = len(chess_board); # The 'M' value of the board.
+            self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1));
 
         def getBoard(self):
             return self.board;
@@ -26,6 +28,9 @@ class BoardState:
         def getAdvPos(self):
             return self.advPosition;
 
+        def getSize(self):
+            return self.board_size;
+
         def getMaxStep(self):
             return self.maxStep;
 
@@ -35,10 +40,57 @@ class BoardState:
             self.advPosition = adv_pos;
             self.maxStep = max_step;
 
+        # Similar to the 'check_endgame()' function from world.py. Returns the amt of points the player has won (0 for loss, 0.5 for tie, 1 for win) or a -1 if it is not the terminal state.
+        def endCheck(self):
+            #    Union-Find
+            father = dict()
+            for r in range(self.board_size):
+                for c in range(self.board_size):
+                    father[(r, c)] = (r, c)
+
+            def find(pos):
+                if father[pos] != pos:
+                    father[pos] = find(father[pos])
+                return father[pos]
+
+            def union(pos1, pos2):
+                father[pos1] = pos2
+
+            for r in range(self.board_size):
+                for c in range(self.board_size):
+                    for dir, move in enumerate(
+                        self.moves[1:3]
+                    ):  # Only check down and right
+                        if self.board[r, c, dir + 1]:
+                            continue
+                        pos_a = find((r, c))
+                        pos_b = find((r + move[0], c + move[1]))
+                        if pos_a != pos_b:
+                            union(pos_a, pos_b)
+
+            for r in range(self.board_size):
+                for c in range(self.board_size):
+                    find((r, c))
+            my_r = find(tuple(self.myPosition)) # Was self.p0_pos in world.py. Replaced with myPos, check here if there's a bug later.
+            adv_r = find(tuple(self.advPosition)) # Was self.p1_pos in world.py. Replaced with advPos, check here if there's a bug later.
+            myScore = list(father.values()).count(my_r)
+            advScore = list(father.values()).count(adv_r)
+            if my_r == adv_r:
+                return -1;
+            if myScore > advScore:
+                return 1;
+            elif myScore < advScore:
+                return 0;
+            else:
+                return 0.5;
+
+
 class MCTSNode:
     def __init__(self):
-        self.nodeState = None;
+        self.boardState = None;
         self.parent = None;
+        self.visitCount = 0; 
+        self.winCount = 0;
         self.childList = [];
 
     def setParent(self, parent):
@@ -47,7 +99,7 @@ class MCTSNode:
     def getParent(self):
         return self.parent;
     
-    def setState(self, state):
+    def setState(self, state: BoardState):
         self.state = state;
 
     def getState(self):
@@ -69,26 +121,6 @@ class MCTSNode:
         # Get the child with the highest 'visitCount' (I think. Review and get max number according to tree policy) ***
         return None;
 
-class MCTSTree:
-    def __init__(self, node: MCTSNode):
-        self.root = node;
-
-    def setRoot(self, node: MCTSNode):
-        self.root = node;
-
-    def getRoot(self):
-        return self.root;
-
-    def addChild(self, parentNode: MCTSNode, childNode: MCTSNode):
-        parentNode.addChildNode(childNode);
-
-class State:
-    
-    def __init__(self, boardState):
-        self.boardState = boardState
-        self.visitCount = 0; #Could put these two in 'Node' Instead, and have just a 'board state'
-        self.winScore = 0;
-
     def getVisitCount(self):
         return self.visitCount;
 
@@ -98,14 +130,14 @@ class State:
     def incrementVisit(self):
         self.visitCount = self.visitCount + 1;
 
-    def getWinScore(self):
-        return self.winScore;
+    def getwinCount(self):
+        return self.winCount;
 
-    def setWinScore(self, newScore):
-        self.winScore = newScore;
+    def setWinCount(self, newScore):
+        self.winCount = newScore;
 
-    def addWinScore(self, scoreToAdd):
-        self.winScore = self.winScore + scoreToAdd;
+    def addwinCount(self, scoreToAdd):
+        self.winCount = self.winCount + scoreToAdd;
 
     def getAllStates(self):
         # Get all possible moves, and return them in a list of States. *** This might end up being very expensive. Edit to be a subset of states if need be.
@@ -115,17 +147,32 @@ class State:
         # Uses BoardState to figure out a random move. Can likely snag random agent code to do this!
         return None;
 
+# class State:
+    
+#     def __init__(self, boardState):
+#         self.boardState = boardState
+#         self.visitCount = 0; #Could put these two in 'Node' Instead, and have just a 'board state'
+#         self.winCount = 0;
+
+#     def getAllStates(self):
+#         # Get all possible moves, and return them in a list of States. *** This might end up being very expensive. Edit to be a subset of states if need be.
+#         return None;
+
+#     def randomMove(self):
+#         # Uses BoardState to figure out a random move. Can likely snag random agent code to do this!
+#         return None;
+
 # Probably doesn't need to be its own class.
 class UCT:
     def findBestUCTNode(node):
         parentVisitCount = node.getState().visitCount();
-        # Foreach child in node.getChildArray, get the one with the highest UCTValue(parentVisitCount, childNode.getState().getWinScore(), childNode.getState().getVisitCount()) ***
+        # Foreach child in node.getChildArray, get the one with the highest UCTValue(parentVisitCount, childNode.getState().getwinCount(), childNode.getState().getVisitCount()) ***
         return None;
 
-    def UCTValue(totalVisits, winScore, visitCountNode): 
+    def UCTValue(totalVisits, winCount, visitCountNode): 
         if(visitCountNode == 0):
             return 9999; #Maybe double check if we can use INTEGER_MAXVAL or something instead.
-        return (winScore / visitCountNode) + 1.41 * math.sqrt(math.log(totalVisits) /  visitCountNode) #Check that this isnt integer division - would cause unpredictable behavior. Also the 'c' value (1.41) can be tuned. ***
+        return (winCount / visitCountNode) + 1.41 * math.sqrt(math.log(totalVisits) /  visitCountNode) #Check that this isnt integer division - would cause unpredictable behavior. Also the 'c' value (1.41) can be tuned. ***
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
@@ -169,16 +216,17 @@ class StudentAgent(Agent):
         timelimit = 1750; # Tried 2000ms (2s) but results came out 2.3s so this seems to be about as high as we can safely go.
         startTime = time.time() * 1000;
         stateList = [];
-
-        targetLevel = 2;
-
+        test = BoardState(chess_board, my_pos, adv_pos, max_step);
+        print(test.endCheck());
         # *** Create a node based on the passed in state, make it the root of the tree
 
         while((time.time() * 1000) < startTime + timelimit):
             #These two lines only here for speed/memory testings
-            copiedState = BoardState(chess_board, my_pos, adv_pos, 2);
+            copiedState = BoardState(chess_board, my_pos, adv_pos, max_step);
             stateList.append(copiedState);
-
+            # curNode = MCTSNode();
+            # initialState = BoardState(chess_board, my_pos, adv_pos, max_step);
+            # curNode.setState(initialState);
             #node = root_node;
             # Step 1: Select a promising node (should be a root at the start)
             # while node is not a leaf
@@ -205,7 +253,7 @@ class StudentAgent(Agent):
 
 """
 CHECKLIST:
-- Need a function to check that a state is terminal (can likely steal from world.py)
+- Need a function to check that a state is terminal (can likely steal from world.py) - DONE!
 - Need function to get possible moves (node expansion)
 - Need a function to simulate random moves until the end
 - Need a function to properly select a UCT node
